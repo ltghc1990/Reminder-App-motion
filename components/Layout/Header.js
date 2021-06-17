38e9d5e2eb4d63e83fd5db4297ee86fb9f1c16dd
Howell <ltghc1990@gmail.com>
2021-06-17 15:11:05 -0400

    default template with tailwindcss

---
+++ components/Layout/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Link from "next/link"
+
+const Header = () => {
+  return (
+    <div className="flex">
+      <Link href="/">
+        <a className="btn">HOME</a>
+      </Link>
+      <nav>
+        <ul>
+          <li className="btn">Tutorials</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Header
